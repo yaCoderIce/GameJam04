d3f149cea1b959fdff3de8a37e7ac3ad8fc9aed6
BriarJamieson <opgirlww@hotmail.com>
2018-02-24 16:15:03 -0500

    BJ Spawning Test1

---
+++ Assets/Spawning_bj_test.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawning_bj_test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
