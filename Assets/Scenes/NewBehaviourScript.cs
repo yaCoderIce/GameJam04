92747cf1555c28569f4255ca3a950bba7d4e3523
Visal <visal.chea.dc@gmail.com>
2018-02-24 12:22:55 -0500

    test

---
+++ Assets/Scenes/NewBehaviourScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//test visal
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
