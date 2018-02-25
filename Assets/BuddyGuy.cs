06dbd3502e2bf50927e444b72c8f64123d9bf5e6
yaCoderIce <samson_y_f_chung@hotmail.com>
2018-02-25 11:22:17 -0500

    Crates are ready

--- Assets/BuddyGuy.cs
+++ Assets/BuddyGuy.cs
@@ -1,31 +1,14 @@
-<<<<<<< HEAD
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class BuddyGuy : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		// Tyler Wrote this
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-}
-=======
-﻿using System;
+﻿﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BuddyGuy : MonoBehaviour {
 
+	public int treasureScore;
     // Use this for initialization
     void Start () {
-		// Tyler Wrote this
+		treasureScore = 0;
 	}
 	
 	// Update is called once per frame
@@ -36,21 +19,28 @@ public class BuddyGuy : MonoBehaviour {
 	void OnCollisionEnter (Collision col){
 		if (col.gameObject.name == "crate"){
 			GameObject chest = GameObject.Find("treasure_chest_closed");
-			for (int i = 0; i < 3; i++){
-				Debug.Log(col.gameObject.transform.rotation);
+			Debug.Log(col.gameObject.transform.position);
+			int numOfTreasures = (int) UnityEngine.Random.Range(1.0f,8.0f);
+			Debug.Log("Number of Treasures: " + numOfTreasures);
+			for (int i = 1; i <= numOfTreasures; i++){
+				
 				Quaternion boo = col.gameObject.transform.rotation;
-				boo = this.transform.rotation;
 				
-				Vector3 pos = new Vector3(col.gameObject.transform.position.x, col.gameObject.transform.position.y);
-				pos = this.transform.position;
-				Instantiate(chest ,pos, boo);
+				
+				Vector3 pos = new Vector3(col.gameObject.transform.position.x, Mathf.Abs(col.gameObject.transform.position.y) * (2 * i), col.gameObject.transform.position.z);
+				Debug.Log(pos);
+				GameObject newChest = Instantiate(chest ,pos, boo);
+				newChest.name = "treasure_chest_closed";
 				
 
 
-				new WaitForSeconds(1);
+				//new WaitForSeconds(1);
 			}
+			Destroy(col.gameObject);
+		} else if (col.gameObject.name == "treasure_chest_closed"){
+			//Count scores
+			this.treasureScore++;
 			Destroy(col.gameObject);
 		}
 	}
 }
->>>>>>> master
