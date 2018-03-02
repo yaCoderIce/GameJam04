3cd971307842f32d6fbc924a46cd806b00999e6e
Visal <visal.chea.dc@gmail.com>
2018-03-01 19:38:52 -0500

    new texMex

---
+++ Assets/EndScoreDisplay.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndScoreDisplay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		this.Text = BuddyGuy.treasureScore;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
